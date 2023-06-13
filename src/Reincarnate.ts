f01bfa0f30720b3481c6076260b9424cd0972441
Vaughn Royko <drathy@gmail.com>
2023-06-12 17:31:40 -0700

    Update for 2.13.2-beta

--- src/Reincarnate.ts
+++ src/Reincarnate.ts
@@ -1,3 +1,14 @@
+/*!
+ * Copyright 2011-2023 Unlok
+ * https://www.unlok.ca
+ *
+ * Credits & Thanks:
+ * https://www.unlok.ca/credits-thanks/
+ *
+ * Wayward is a copyrighted and licensed work. Modification and/or distribution of any source files is prohibited. If you wish to modify the game in any way, please refer to the modding guide:
+ * https://github.com/WaywardGame/types/wiki
+ */
+
 import { SfxType } from "audio/IAudio";
 import { EventBus } from "event/EventBuses";
 import { EventHandler } from "event/EventManager";
@@ -74,7 +85,7 @@ export default class Reincarnate extends Mod {
 		player.movementCompleteZ = undefined;
 		player.restData = undefined;
 		player.swimming = false;
-		player.stopNextMovement = false;
+		delete player.shouldSkipNextMovement;
 
 		// Random character
 		player.customization = {
